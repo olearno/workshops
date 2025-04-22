575e06712f1052388dea8ae954b7be110fcd3fd2
Ruhulla Sheik <ruhulla.sheik@amsoftware.com>
2025-04-22 16:00:05 +0530

    Updates schema

--- mongo/task-manager/server.js
+++ mongo/task-manager/server.js
@@ -13,7 +13,9 @@ mongoose.connect("mongodb://localhost:27017/taskdb", {
 // Define Task Schema & Model
 const taskSchema = new mongoose.Schema({
     task: String,
-    status: String
+    status: String,
+	priority: String,
+	dueDate: String
 });
 const Task = mongoose.model("Task", taskSchema);
 
